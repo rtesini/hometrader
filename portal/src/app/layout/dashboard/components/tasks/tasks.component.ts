c50e388bd6dbdcbc592459550e8d7cda0dd33a12
danilodeLuca <danilo.pereiradeluca@gmail.com>
2017-07-29 06:15:37 -0300

    rename tasks

--- portal/src/app/layout/dashboard/components/tasks/tasks.component.ts
+++ portal/src/app/layout/dashboard/components/tasks/tasks.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TasksComponent implements OnInit {
     
     public showInfo = false;
-    public info = {};
+    public info = { name: '', status: ''};
 
     constructor() { 
     	console.log('hello');
